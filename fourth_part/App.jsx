9b8727cf4d6f4d048a2c26a57455c4db49352715
Md.Miner Hossain Rimon <minerhossainrimon1033@gmail.com>
2025-05-05 19:11:20 +0600

    Add files via upload

---
+++ fourth_part/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Person from './Person';
+import Superman from './Superman';
+
+const App = () => {
+  return (
+    <>
+      <Superman/>
+    </>
+  );
+};
+
+export default App;
