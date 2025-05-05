14747c39104668cce3005c76307f846694c8cad5
Md.Miner Hossain Rimon <minerhossainrimon1033@gmail.com>
2025-05-05 18:47:01 +0600

    Add files via upload

--- Second_part/App.jsx
+++ Second_part/App.jsx
@@ -15,7 +15,7 @@ function App() {
   // Declaring a variable named 'age'
   const age = 20;
 
-  // Creating a person object with multiple properties
+  // Creating a 'person' object with multiple personal details
   const person = {
     name: 'Miner',
     age: 23,
@@ -23,39 +23,49 @@ function App() {
     mobile_number: 1763699850
   };
 
+  // Creating a 'product' object with product details
+  const product = {
+    title: "iphone",
+    model: 'iphone-15',
+    price: 70000
+  };
+
   // Returning JSX (UI) to be rendered on the screen
   return (
     <>
-      {/* React Fragment used to group multiple elements without a wrapper div */}
+      {/* React Fragment used to group multiple elements without adding extra <div> in HTML */}
 
-      <div> {/* A container div for the main content */}
+      <div> {/* First container div */}
         <h1>
-          This is react course {/* A heading element */}
+          This is react course {/* Heading for the page */}
         </h1>
 
-        <Hello></Hello> {/* Rendering the Hello component */}
-        <Random></Random> {/* Rendering the Random component */}
+        <Hello></Hello> {/* Rendering Hello component */}
+        <Random></Random> {/* Rendering Random component */}
 
-        {/* KgButton is imported but not used in this file */}
+        {/* KgButton is imported but still not used - can be added below if needed */}
 
-        {/* These lines are currently commented out, used to test variable output */}
+        {/* These lines are commented out - they show examples of using variables in JSX */}
         {/* <h1>{2+4}</h1>
         <h1>{name}</h1>
         <h1>age={age}</h1> */}
 
-        {/* Displaying values from the 'person' object */}
+        {/* Displaying data from 'person' object */}
         <h1>Name = {person.name}</h1>
         <h1>Age = {person.age}</h1>
         <h1>Gmail = {person.gmail}</h1>
         <h1>Mobile Number = {person.mobile_number}</h1>
       </div>
 
-      <div> {/* Another separate div section */}
-        <h1>Hello</h1> {/* A simple heading */}
+      <div> {/* Second container div */}
+        {/* Displaying data from 'product' object */}
+        <h3>Title = {product.title}</h3>
+        <h3>Model = {product.model}</h3>
+        <h4>Price = {product.price}</h4>
       </div>
     </>
   );
 }
 
-// Exporting the App component so it can be used in index.js or main.jsx
+// Exporting the App component to be used in index.js or main.jsx
 export default App;
